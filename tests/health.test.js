4c80ce946839d743fbdc753867dbc02237599a30
ashrafakib02 <ashrafakib02@gmail.com>
2026-03-13 16:06:03 +0600

    Add CI pipeline with GitHub Actions

---
+++ tests/health.test.js
@@ -0,0 +1,11 @@
+import request from "supertest";
+import app from "../src/app.js";
+
+describe("Health endpoint", () => {
+  it("should return server health", async () => {
+    const res = await request(app).get("/api/v1/health");
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.message).toBe("Server is healthy from get health");
+  });
+});
